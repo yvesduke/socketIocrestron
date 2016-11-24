b1863d0474fc75bfb2cb1020daa9466f599942de
Yves Dukuze <yvesduke06@live.co.uk>
2016-11-24 14:55:05 +0000

    added client

--- index.js
+++ index.js
@@ -2,21 +2,16 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+aapp.get('/', function(req, res){
+  res.sendfile('index.html');
 });
 
 io.on('connection', function(socket){
-  io.emit('some event', { for: 'everyone' });
-  // socket.on('disconnect', function(){
- // });
+   socket.on('chat message', function(msg){
+   io.emit('chat message', msg);
+    //console.log('message: ' + msg);
+  });
 });
-
-//io.on('connection', function(socket){
-   //socket.on('chat message', function(msg){
- //  io.emit('chat message', msg);
- // });
-//});
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
